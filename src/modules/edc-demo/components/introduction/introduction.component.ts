cdeed0608f828a22da3c4df9a31a2362ba300eed
Athul Mohan <mohanathul71@gmail.com>
2023-11-09 12:26:30 +0530

    transfer process dashboard

--- src/modules/edc-demo/components/introduction/introduction.component.ts
+++ src/modules/edc-demo/components/introduction/introduction.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import { ContractAgreement, ContractDefinition, PolicyDefinition } from '@think-it-labs/edc-connector-client';
 import { Asset } from '@think-it-labs/edc-connector-client/dist/src/entities/asset';
 import { Observable, map, of } from 'rxjs';
-import { AssetService, ContractAgreementService, ContractDefinitionService, PolicyService } from 'src/modules/mgmt-api-client';
+import { AssetService, ContractAgreementService, ContractDefinitionService, PolicyService, TransferProcessService } from 'src/modules/mgmt-api-client';
 
 @Component({
   selector: 'edc-demo-introduction',
@@ -11,19 +11,20 @@ import { AssetService, ContractAgreementService, ContractDefinitionService, Poli
 })
 export class IntroductionComponent implements OnInit{
 
-
+  transferProcesses$: Observable<any[]> = of([]);
   dataOfferingCount$: Observable<number> = of(0);
   assetCount$: Observable<number> = of(0);
   policyCount$: Observable<number> = of(0);
   preconfiguredCatalogCount: number =0;
   contractAgreementCount$: Observable<number> = of(0);
+  columns: string[] = ['id', 'state', 'connectorId', 'assetId'];
 
-  constructor(private assetService:AssetService, private policyService:PolicyService,
+  constructor(private assetService:AssetService, private policyService:PolicyService,private transferProcessService:TransferProcessService,
      private contractDefinitionService:ContractDefinitionService, private contractAgreementService:ContractAgreementService){
   }
 
   ngOnInit(): void {
-
+    this.loadTransferProcesses();
    this.assetCount$ = this.assetService.requestAssets().pipe(
       map((assetArray: Asset[])=>{
         return assetArray.length;
@@ -53,5 +54,7 @@ export class IntroductionComponent implements OnInit{
   }
 
 
-
+  loadTransferProcesses() {
+    this.transferProcesses$ = this.transferProcessService.queryAllTransferProcesses();
+ }
 }
